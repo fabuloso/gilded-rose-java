5f613412fe231ba89099841a75e49959241bf770
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-11-19 14:17:03 +0100

    Quality for Conjured item can never go below 0

--- rose/Java/com/gildedrose/GildedRoseTest.java
+++ rose/Java/com/gildedrose/GildedRoseTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     private static final int QUALITY = 80;
+    private static final int LOWEST_QUALITY = 0;
 
 	@Test
     public void conjured_item_quality_degrades_by_one_before_sell_in() {
@@ -26,6 +27,16 @@ public class GildedRoseTest {
         app.updateQuality();
         
         assertEquals(QUALITY - 4, app.items[0].quality);
+    }
+    
+    @Test
+    public void conjured_item_quality_never_goes_below_0() {
+        Item[] items = new Item[] { new Item("Conjured", 0, LOWEST_QUALITY) };
+        GildedRose app = new GildedRose(items);
+        
+        app.updateQuality();
+        
+        assertEquals(LOWEST_QUALITY, app.items[0].quality);
     }
 
 }
