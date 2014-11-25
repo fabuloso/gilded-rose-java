4b3089504265cc27c1521ca0e20504b40139b1de
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-11-25 21:32:18 +0100

    Improving tests

--- rose/Java/com/gildedrose/GildedRoseTest.java
+++ rose/Java/com/gildedrose/GildedRoseTest.java
@@ -12,7 +12,7 @@ public class GildedRoseTest {
     private static final int SELL_IN = 1;
 
 	@Test
-    public void conjured_item_quality_degrades_by_one_before_sell_in() {
+    public void conjured_quality_degrades_by_one_before_sell_in() {
         Item[] items = new Item[] { new Item("Conjured", SELL_IN, QUALITY) };
         GildedRose app = new GildedRose(items);
         
@@ -22,7 +22,7 @@ public class GildedRoseTest {
     }
     
     @Test
-    public void conjured_item_quality_degrades_by_two_after_sell_in() {
+    public void conjured_quality_degrades_by_two_after_sell_in() {
         Item[] items = new Item[] { new Item("Conjured", 0, QUALITY) };
         GildedRose app = new GildedRose(items);
         
@@ -32,7 +32,7 @@ public class GildedRoseTest {
     }
     
     @Test
-    public void conjured_item_quality_never_goes_below_0() {
+    public void conjured_quality_never_goes_below_0() {
         Item[] items = new Item[] { new Item("Conjured", 0, LOWEST_QUALITY) };
         GildedRose app = new GildedRose(items);
         
@@ -42,7 +42,7 @@ public class GildedRoseTest {
     }
     
 	@Test
-    public void conjured_item_sell_in_goes_down_of_1_after_update() {
+    public void conjured_sell_in_goes_down_of_1_after_update() {
         Item[] items = new Item[] { new Item("Conjured", SELL_IN, QUALITY) };
         GildedRose app = new GildedRose(items);
         
@@ -52,7 +52,7 @@ public class GildedRoseTest {
     }
     
 	@Test
-    public void sulfuras_item_is_always_80() {
+    public void sulfuras_quality_is_always_80() {
         Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", SELL_IN, QUALITY) };
         GildedRose app = new GildedRose(items);
         
