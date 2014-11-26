b94dbfc3c7bbe7ce5efc4a7cbaa3743da2ccac57
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-11-26 14:19:17 +0100

    Extracting sell in decrease constant and refactoring methods on conjured item

--- rose/Java/com/gildedrose/GildedRose.java
+++ rose/Java/com/gildedrose/GildedRose.java
@@ -1,73 +1,73 @@
 package com.gildedrose;
 
 class GildedRose {
-	Item[] items;
+    private static final int SELL_IN_DECREASE = 1;
+    Item[] items;
 
-	public GildedRose(Item[] items) {
-		this.items = items;
-	}
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
 
-	public void updateQuality() {
-		for (Item item : items) {
-			if (item.name.equals("Conjured")) {
-				if (item.quality > 0) {
-					if (item.sellIn > 0) {
-						item.quality = item.quality - 2;
-					} else {
-						item.quality = item.quality - 4;
-					}
-					item.sellIn -= 1;
-				}
-				continue;
-			}
+    public void updateQuality() {
+        for (Item item : items) {
+            if (item.name.equals("Conjured")) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 2 * SELL_IN_DECREASE;
+                    if (item.sellIn <= 0) {
+                        item.quality = item.quality - 2 * SELL_IN_DECREASE;
+                    }
+                    item.sellIn -= SELL_IN_DECREASE;
+                }
+                continue;
+            }
 
-			if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-				continue;
-			}
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                continue;
+            }
 
-			item.sellIn = item.sellIn - 1;
+            item.sellIn = item.sellIn - 1;
 
-			if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (item.quality > 0) {
-					if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-						item.quality = item.quality - 1;
-					}
-				}
-			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
+            if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-					if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (item.sellIn < 11) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
 
-						if (item.sellIn < 6) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
-					}
-				}
-			}
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
 
-			if (item.sellIn < 0) {
-				if (!item.name.equals("Aged Brie")) {
-					if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (item.quality > 0) {
-							item.quality = item.quality - 1;
-						}
-					} else {
-						item.quality = item.quality - item.quality;
-					}
-				} else {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-				}
-			}
-		}
-	}
+            if (item.sellIn < 0) {
+                if (!item.name.equals("Aged Brie")) {
+                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.quality > 0) {
+                            item.quality = item.quality - 1;
+                        }
+                    } else {
+                        item.quality = item.quality - item.quality;
+                    }
+                } else {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        }
+    }
 }
