d2d480c85636660ac13937c5f6b074758f3e001f
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-11-29 19:18:30 +0100

    Extracting logic for aged brie item in separate if

--- rose/Java/com/gildedrose/GildedRose.java
+++ rose/Java/com/gildedrose/GildedRose.java
@@ -25,9 +25,20 @@ class GildedRose {
                 continue;
             }
 
+            if (item.name.equals("Aged Brie")) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                    if (item.sellIn <= 0) {
+                        item.quality = item.quality + 1;
+                    }
+                    item.sellIn -= STANDARD_SELL_IN_DECREASE;
+                }
+                continue;
+            }
+
             item.sellIn = item.sellIn - STANDARD_SELL_IN_DECREASE;
 
-            if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality > 0) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                         item.quality = item.quality - 1;
@@ -54,18 +65,12 @@ class GildedRose {
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
+                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    item.quality = item.quality - item.quality;
                 }
             }
         }
--- rose/Java/com/gildedrose/GildedRoseTest.java
+++ rose/Java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,6 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -11,63 +11,103 @@ public class GildedRoseTest {
 
     private static final int SELL_IN = 1;
 
-	@Test
+    @Test
     public void conjured_quality_degrades_by_one_before_sell_in() {
         Item[] items = new Item[] { new Item("Conjured", SELL_IN, QUALITY) };
         GildedRose app = new GildedRose(items);
-        
+
         app.updateQuality();
-        
+
         assertEquals(QUALITY - 2, app.items[0].quality);
     }
-    
+
     @Test
     public void conjured_quality_degrades_by_two_after_sell_in() {
         Item[] items = new Item[] { new Item("Conjured", 0, QUALITY) };
         GildedRose app = new GildedRose(items);
-        
+
         app.updateQuality();
-        
+
         assertEquals(QUALITY - 4, app.items[0].quality);
     }
-    
+
     @Test
     public void conjured_quality_never_goes_below_0() {
         Item[] items = new Item[] { new Item("Conjured", 0, LOWEST_QUALITY) };
         GildedRose app = new GildedRose(items);
-        
+
         app.updateQuality();
-        
+
         assertEquals(LOWEST_QUALITY, app.items[0].quality);
     }
-    
-	@Test
+
+    @Test
     public void conjured_sell_in_goes_down_of_1_after_update() {
         Item[] items = new Item[] { new Item("Conjured", SELL_IN, QUALITY) };
         GildedRose app = new GildedRose(items);
-        
+
         app.updateQuality();
-        
+
         assertEquals(SELL_IN - 1, app.items[0].sellIn);
     }
-    
-	@Test
+
+    @Test
     public void sulfuras_quality_is_always_80() {
         Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", SELL_IN, QUALITY) };
         GildedRose app = new GildedRose(items);
-        
+
         app.updateQuality();
-        
+
         assertEquals(QUALITY, app.items[0].quality);
     }
-	
-	@Test
+
+    @Test
     public void sulfuras_sell_in_never_decreases() {
         Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", SELL_IN, QUALITY) };
         GildedRose app = new GildedRose(items);
-        
+
         app.updateQuality();
-        
+
         assertEquals(SELL_IN, app.items[0].sellIn);
+    }
+
+    @Test
+    public void aged_brie_quality_icreases_by_one_before_sell_in() throws Exception {
+        Item[] items = new Item[] { new Item("Aged Brie", SELL_IN, 49) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    public void aged_brie_quality_icreases_by_two_after_sell_in() throws Exception {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 48) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    public void aged_brie_maximum_quality_is_50() throws Exception {
+        Item[] items = new Item[] { new Item("Aged Brie", SELL_IN, 50) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    public void aged_brie_sell_in_goes_down_of_1_after_update() throws Exception {
+        Item[] items = new Item[] { new Item("Aged Brie", SELL_IN, 49) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(SELL_IN - 1, app.items[0].sellIn);
     }
 }
