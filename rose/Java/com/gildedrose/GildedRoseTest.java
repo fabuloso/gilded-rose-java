515d01735d684ebab9c4469d35935c3aa78b582b
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-11-30 16:47:09 +0100

    Removing not anymore used test for conjured

--- rose/Java/com/gildedrose/GildedRoseTest.java
+++ rose/Java/com/gildedrose/GildedRoseTest.java
@@ -7,49 +7,8 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     private static final int QUALITY = 80;
-    private static final int LOWEST_QUALITY = 0;
 
     private static final int SELL_IN = 1;
-
-    @Test
-    public void conjured_quality_degrades_by_one_before_sell_in() {
-        Item[] items = new Item[] { new Item("Conjured", SELL_IN, QUALITY) };
-        GildedRose app = new GildedRose(items);
-
-        app.updateQuality();
-
-        assertEquals(QUALITY - 2, app.items[0].quality);
-    }
-
-    @Test
-    public void conjured_quality_degrades_by_two_after_sell_in() {
-        Item[] items = new Item[] { new Item("Conjured", 0, QUALITY) };
-        GildedRose app = new GildedRose(items);
-
-        app.updateQuality();
-
-        assertEquals(QUALITY - 4, app.items[0].quality);
-    }
-
-    @Test
-    public void conjured_quality_never_goes_below_0() {
-        Item[] items = new Item[] { new Item("Conjured", 0, LOWEST_QUALITY) };
-        GildedRose app = new GildedRose(items);
-
-        app.updateQuality();
-
-        assertEquals(LOWEST_QUALITY, app.items[0].quality);
-    }
-
-    @Test
-    public void conjured_sell_in_goes_down_of_1_after_update() {
-        Item[] items = new Item[] { new Item("Conjured", SELL_IN, QUALITY) };
-        GildedRose app = new GildedRose(items);
-
-        app.updateQuality();
-
-        assertEquals(SELL_IN - 1, app.items[0].sellIn);
-    }
 
     @Test
     public void aged_brie_quality_icreases_by_one_before_sell_in() throws Exception {
