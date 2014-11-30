2c6e3e741539caa02d68640039844144ab3e0eb9
Paolo D'Incau <paolo.dincau@xpeppers.com>
2014-11-30 15:52:09 +0100

    Improving strategy code

--- rose/Java/com/gildedrose/AgedBrieUpdateStrategy.java
+++ rose/Java/com/gildedrose/AgedBrieUpdateStrategy.java
@@ -8,12 +8,17 @@ public class AgedBrieUpdateStrategy {
 
     public void update(Item item) {
         if (item.quality < MAXIMUM_QUALITY) {
-            item.quality += QUALITY_INCREASE;
-            if (item.sellIn <= 0) {
-                item.quality += QUALITY_INCREASE;
-            }
+            updateQuality(item);
         }
         decreaseSellIn(item);
+    }
+
+    private void updateQuality(Item item) {
+        int increaseFactor = 1;
+        if (item.sellIn <= 0) {
+            increaseFactor += 1;
+        }
+        item.quality += increaseFactor * QUALITY_INCREASE;
     }
 
     private void decreaseSellIn(Item item) {
